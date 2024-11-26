ce493e3ad8aed0ea5813ece9eaa14e650b5f1b81
Andrius Slunka <andrius.slunka@gmail.com>
2024-11-26 16:42:47 +0000

    added ex.4

--- F1.py
+++ F1.py
@@ -3,4 +3,4 @@ lap_count = 45
 min_requirement = fuel_per_lap * lap_count
 contingency = min_requirement * 0.5
 total_fuel = min_requirement + contingency
-print(min_requirement, contingency, total_fuel)
+
