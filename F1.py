3bf7ce9cdef3fdc76d2b6719240676614245d5a3
Andrius Slunka <andrius.slunka@gmail.com>
2024-11-26 16:48:37 +0000

    added ex.4

--- F1.py
+++ F1.py
@@ -2,5 +2,13 @@ fuel_per_lap = 2.25 #kg
 lap_count = 45
 min_requirement = fuel_per_lap * lap_count
 contingency = min_requirement * 0.5
-total_fuel = min_requirement + contingency
+fuel_at_start = min_requirement + contingency
+
+qualify_lap = 80.45
+time_loss = fuel_at_start / 10 * 0.35
+
+print("time lost at start:", time_loss)
+
+first_lap_time = qualify_lap + time_loss
+print("first lapt time:", first_lap_time)
 
