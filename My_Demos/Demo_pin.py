617f304a6a38062e0af0eb64be4f8a0bec824130
Andrius Slunka <andrius.slunka@gmail.com>
2024-11-27 11:20:31 +0000

    added get pass

--- My_Demos/Demo_pin.py
+++ My_Demos/Demo_pin.py
@@ -2,12 +2,14 @@
 # Author: ASlunka
 # Description: This script will simulate a high street bank PIN machine
 
+import getpass
+
 master_pin = "0123"
 pin = None #undifined but declared
 attempts = 0
 
 while pin != master_pin and attempts <3: 
-    pin = input("Enter PIN:" )
+    pin = getpass.getpass("Enter PIN:" )
     if pin == master_pin:
         print("Valid PIN")
         break
