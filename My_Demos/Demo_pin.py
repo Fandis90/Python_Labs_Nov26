5939e14bb8a1a312ec144399dff2f93ab672e38f
Andrius Slunka <andrius.slunka@gmail.com>
2024-11-27 10:51:00 +0000

    Added script to simulate a pin entry

---
+++ My_Demos/Demo_pin.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+# Author: ASlunka
+# Description: This script will simulate a high street bank PIN machine
+
+master_pin = "0123"
+pin = None #undifined but declared
+attempts = 0
+
+while pin != master_pin and attempts <3: 
+    pin = input("Enter PIN:" )
+    if pin == master_pin:
+        print("Valid PIN")
+        break
+    else:
+        print("Invalid PIN")
+        attempts += 1
+else:
+    # Executes only once, when while condition becomes False
+    print("Too many attempts")
+    print("Your card has been retained. Have a nice day")
+
+print("Done.")
